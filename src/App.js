8eac93ab1363fa55cfb296777b62cc345abb0afe
Thaís <fukudathais@gmail.com>
2020-03-12 23:18:09 -0300

    📍 setup app routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+
 import './styles/App.scss';
 
+import Home from './pages/Home';
+import Category from './pages/Category';
+
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          ChuckNorrify
-          <img src="./chuck-norris.gif" className="App-logo" alt="logo" />
-        </h1>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <main className="App">
+                <Switch>
+                    <Route exact path="/" component={Home} />
+
+                    <Route path="/category/:category" component={Category} />
+                </Switch>
+            </main>
+        </Router>
+    );
 }
 
 export default App;
