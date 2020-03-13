d9d0d2c846bfbcb71a906178007282cbbc83a284
Thaís <fukudathais@gmail.com>
2020-03-13 00:42:04 -0300

    ❤️ update footer link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,12 @@
 
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { TiHeart } from 'react-icons/ti';
 
 class Footer extends Component {
     render() {
         return (
             <footer className="App-footer">
-                made with <TiHeart /> by <Link to="https://github.com/fukudathais">Thais Fukuda</Link>
+                made with <TiHeart /> by <a href="https://github.com/fukudathais">Thais Fukuda</a>
             </footer>
         );
     }
